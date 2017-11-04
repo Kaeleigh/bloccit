cec1fcdd89a71d0dfa7e989649629af33ed75d8a
[] <awh938@gmail.com>
2017-11-03 23:04:55 -0400

    faq view added

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe WelcomeController, type: :controller do
   describe "GET index" do
     it "renders the index template" do
       # #2 get calls the index method of controller
-      get:index
+      get :index
       # #3 expect the controller's response to show the index template
       expect(response).to render_template("index")
     end
@@ -15,6 +15,13 @@ RSpec.describe WelcomeController, type: :controller do
      it "renders the about template" do
        get :about
        expect(response).to render_template("about")
+     end
+   end
+
+   describe "GET faq" do
+     it "renders the faq template" do
+       get :faq
+       expect(response).to render_template("faq")
      end
    end
 
