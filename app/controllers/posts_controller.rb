a841e80a72202ce31b0788de74c45b37c6a54750
[] <awh938@gmail.com>
2017-11-08 19:30:55 -0500

    SPAM added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,12 @@ class PostsController < ApplicationController
   def index
     # declare instance var @posts and assign it collection of Post obj
     @posts = Post.all
+    # loop iterate through posts, each 5th post is marked SPAM
+    @posts.each_with_index do |post, index|
+      if index % 5 == 0
+        post.title = "SPAM"
+      end
+    end
   end
 
   def show
