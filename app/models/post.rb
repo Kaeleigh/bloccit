8c8444f16b4ded670cf4ac24e2ce390ffd128b12
[] <awh938@gmail.com>
2017-11-26 10:47:46 -0500

    assign_26 complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,8 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   default_scope {order('created_at DESC') }
+  scope:ordered_by_title, -> { order('title DESC') }
+  scope:ordered_by_reverse_created_at, -> { order('created_at ASC') }
   # validates title is at least 5 and body is at least 20
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
