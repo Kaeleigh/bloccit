22e8a905df747f9d97b01889b59c8cb2d32b2fa4
[] <awh938@gmail.com>
2017-12-02 21:43:25 -0500

    assign_30 done

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -3,17 +3,29 @@ class FavoriteMailer < ApplicationMailer
 
   def new_comment(user, post, comment)
 
- # #three headers set to enable conversation
-     headers["Message-ID"] = "<comments/#{comment.id}@akwbloccit.example>"
-     headers["In-Reply-To"] = "<post/#{post.id}@akwbloccit.example>"
-     headers["References"] = "<post/#{post.id}@akwbloccit.example>"
+    # #three headers set to enable conversation
+    headers["Message-ID"] = "<comments/#{comment.id}@akwbloccit.example>"
+    headers["In-Reply-To"] = "<post/#{post.id}@akwbloccit.example>"
+    headers["References"] = "<post/#{post.id}@akwbloccit.example>"
 
-     @user = user
-     @post = post
-     @comment = comment
+    @user = user
+    @post = post
+    @comment = comment
 
- # #mail method preps email with info in hash
-     mail(to: user.email, subject: "New comment on #{post.title}")
-   end
+    # #mail method preps email with info in hash
+    mail(to: user.email, subject: "New comment on #{post.title}")
+  end
+
+  def new_post(post)
+
+    # #three headers set to enable conversation
+    headers["Message-ID"] = "<posts/#{post.id}@akwbloccit.example>"
+    headers["In-Reply-To"] = "<post/#{post.id}@akwbloccit.example>"
+    headers["References"] = "<post/#{post.id}@akwbloccit.example>"
+
+    @post = post
+
+    mail(to: post.user.email, subject: "You are following, #{post.title}!")
+  end
 # ends class
 end
