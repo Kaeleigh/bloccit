ea5f6767663cabf58a6fafe2eb15f219a9790622
[] <awh938@gmail.com>
2017-12-05 20:16:58 -0500

    helper created for users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+  def users_has_no_post?(user)
+    user.posts.count == 0
+  end
+
+  def users_has_no_comment?(user)
+    user.comments.count == 0
+  end
+
+  def users_has_no_favorite?(user)
+    user.favorites.count == 0
+  end
 end
